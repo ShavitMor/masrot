6f785679e54d9c975fde5ee3cb72f1ae9c203bb4
ShavitMor <102467231+ShavitMor@users.noreply.github.com>
2024-11-07 00:14:43 +0200

    update ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import CalendarComponent from './components/CalendarComponent';
-import NavbarComponent from './components/NavbarComponent';
 import ExpenseEntryPage from './components/ExpenseEntryPage'; // Page for entering expenses and salaries
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <NavbarComponent />
         <Routes>
           <Route path="/" element={<CalendarComponent />} />
           <Route path="/expenses/:date" element={<ExpenseEntryPage />} />
